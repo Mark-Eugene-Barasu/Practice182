acb5be142dcec551112edc324f5c2435823d64a8
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-13 04:26:24 +0200

    checking memory location

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # list
 my_list = [1, 2, 3, 4, 5]
 print(my_list)
@@ -10,9 +11,16 @@ print(my_list[-3])  # Accessing the third element from the end
 
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
-print(bicycles[0].lower())  # Accessing the first element
-print(bicycles[1].lower())  # Accessing the second element
-print(bicycles[2].lower())  # Accessing the third element
-print(bicycles[3].lower())  # Accessing the fourth element
-print(bicycles[-1].lower())  # Accessing the last element
-print(bicycles[-2].lower())  # Accessing the second to last element
+print(bicycles[0].title())  # Accessing the first element
+print(bicycles[1].title())  # Accessing the second element
+print(bicycles[2].title())  # Accessing the third element
+print(bicycles[3].title())  # Accessing the fourth element
+print(bicycles[-1].title())  # Accessing the last element
+print(bicycles[-2].title())  # Accessing the second to last element
+
+bicycles[3] = "mountain"
+print(bicycles)
+
+# Checking if the fourth element is equal to the last element
+print(bicycles[3].title() == bicycles[-1].title())
+print(bicycles[3].title() is bicycles[-1].title())
