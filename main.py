71fc4e2bf19f8227859824ab5e5bc03c46059c64
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-13 04:10:38 +0200

    changing everything bicycle output into uppercase

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ print(my_list[-3])  # Accessing the third element from the end
 
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
-print(bicycles[0])  # Accessing the first element
-print(bicycles[1])  # Accessing the second element
-print(bicycles[2])  # Accessing the third element
-print(bicycles[3])  # Accessing the fourth element
-print(bicycles[-1])  # Accessing the last element
-print(bicycles[-2])  # Accessing the second to last element
-
+print(bicycles[0].upper())  # Accessing the first element
+print(bicycles[1].upper())  # Accessing the second element
+print(bicycles[2].upper())  # Accessing the third element
+print(bicycles[3].upper())  # Accessing the fourth element
+print(bicycles[-1].upper())  # Accessing the last element
+print(bicycles[-2].upper())  # Accessing the second to last element
