ccc2d5e1fb8237045f5c6711647efc8f401136a2
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-13 04:08:01 +0200

    manipulting the list

---
+++ main.py
@@ -0,0 +1,19 @@
+# list
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+
+print(my_list[0])  # Accessing the first element
+print(my_list[2])  # Accessing the third element
+print(my_list[-1])  # Accessing the last element
+print(my_list[-3])  # Accessing the third element from the end
+
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])  # Accessing the first element
+print(bicycles[1])  # Accessing the second element
+print(bicycles[2])  # Accessing the third element
+print(bicycles[3])  # Accessing the fourth element
+print(bicycles[-1])  # Accessing the last element
+print(bicycles[-2])  # Accessing the second to last element
+
