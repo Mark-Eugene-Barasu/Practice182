04e441c2b7e2f270defdb6251acacf58773b939e
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-13 07:23:27 +0200

    using .append() and .pop() in python

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
+# creating a list of car brands
+cars = ["Toyota", "Honda", "Ford", "BMW", "Mercedes"]
 
-# list
-my_list = [1, 2, 3, 4, 5]
-print(my_list)
+# Accessing the first element using index position 0
+first_car = cars[0]
+print(
+    f"The first car brand is: {first_car}")
 
-print(my_list[0])  # Accessing the first element
-print(my_list[2])  # Accessing the third element
-print(my_list[-1])  # Accessing the last element
-print(my_list[-3])  # Accessing the third element from the end
+cars.append("Audi")
+print(f"Updated list of car brands: {cars}")
 
-
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles)
-print(bicycles[0].title())  # Accessing the first element
-print(bicycles[1].title())  # Accessing the second element
-print(bicycles[2].title())  # Accessing the third element
-print(bicycles[3].title())  # Accessing the fourth element
-print(bicycles[-1].title())  # Accessing the last element
-print(bicycles[-2].title())  # Accessing the second to last element
-
-bicycles[3] = "mountain"
-print(bicycles)
-
-# Checking if the fourth element is equal to the last element
-print(bicycles[3].title() == bicycles[-1].title())
-print(bicycles[3].title() is bicycles[-1].title())
+# Removing the last element from the list
+removed_car = cars.pop()  # Removing the last car brand (Audi)
+print(f"Updated list of car brands: {cars}")
+print(f"Removed car brand: {removed_car}")
+removed_car = cars.pop(2)  # Removing the car brand at index position 2 (Ford)
+print(f"Updated list of car brands: {cars}")
+print(f"Removed car brand: {removed_car}")
