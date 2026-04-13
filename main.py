36df387f91a4ff97d327099e3319b527228a3660
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-13 04:12:04 +0200

    changing all bicycle outputs into lower case, there will be no difference as origial string is already in lowercase

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ print(my_list[-3])  # Accessing the third element from the end
 
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
-print(bicycles[0].upper())  # Accessing the first element
-print(bicycles[1].upper())  # Accessing the second element
-print(bicycles[2].upper())  # Accessing the third element
-print(bicycles[3].upper())  # Accessing the fourth element
-print(bicycles[-1].upper())  # Accessing the last element
-print(bicycles[-2].upper())  # Accessing the second to last element
+print(bicycles[0].lower())  # Accessing the first element
+print(bicycles[1].lower())  # Accessing the second element
+print(bicycles[2].lower())  # Accessing the third element
+print(bicycles[3].lower())  # Accessing the fourth element
+print(bicycles[-1].lower())  # Accessing the last element
+print(bicycles[-2].lower())  # Accessing the second to last element
